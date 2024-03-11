4c48df4d654b89c315ea402fc346e6f34c358d27
Scott Moore (Arch) <scottmoore@archlinux-epsilon.broadband>
2024-03-11 04:33:26 +0000

    Let's send some network data now!

--- wipi-firmware.c
+++ wipi-firmware.c
@@ -13,10 +13,12 @@
 #include "hardware/vreg.h"
 #include "hardware/clocks.h"
 
-#include "lwip/pbuf.h"
-#include "lwip/tcp.h"
-#include "lwip/udp.h"
+#include "lwip/mem.h"
 #include "lwip/raw.h"
+#include "lwip/icmp.h"
+#include "lwip/netif.h"
+#include "lwip/sys.h"
+#include "lwip/inet_chksum.h"
 
 #include "commands.h"
 #include "status.h"
@@ -30,6 +32,104 @@
 #define WIFI_PASSWORD ""
 #endif
 
+/**
+ * PING_DEBUG: Enable debugging for PING.
+ */
+#define PING_DEBUG     LWIP_DBG_ON
+#define PING_TARGET   (netif_default?netif_default->gw:ip_addr_any)
+#define PING_RCV_TIMEO 1000
+#define PING_DELAY     1000
+#define PING_ID        0xAFAF
+#define PING_DATA_SIZE 32
+#define PING_RESULT(ping_ok)
+
+static u16_t ping_seq_num;
+static u32_t ping_time;
+static struct raw_pcb *ping_pcb;
+
+static void
+ping_prepare_echo( struct icmp_echo_hdr *iecho, u16_t len)
+{
+  size_t i;
+  size_t data_len = len - sizeof(struct icmp_echo_hdr);
+
+  ICMPH_TYPE_SET(iecho, ICMP_ECHO);
+  ICMPH_CODE_SET(iecho, 0);
+  iecho->chksum = 0;
+  iecho->id     = PING_ID;
+  iecho->seqno  = htons(++ping_seq_num);
+
+  /* fill the additional data buffer with some data */
+  for(i = 0; i < data_len; i++) {
+    ((char*)iecho)[sizeof(struct icmp_echo_hdr) + i] = (char)i;
+  }
+
+  iecho->chksum = inet_chksum(iecho, len);
+}
+
+
+static u8_t
+ping_recv(void *arg, struct raw_pcb *pcb, struct pbuf *p, const ip_addr_t *addr)
+{
+    struct icmp_echo_hdr *iecho;
+    LWIP_UNUSED_ARG(arg);
+    LWIP_UNUSED_ARG(pcb);
+    LWIP_UNUSED_ARG(addr);
+    LWIP_ASSERT("p != NULL", p != NULL);
+
+    printf("static u8_t ping_recv(void *arg, struct raw_pcb *pcb, struct pbuf *p, const ip_addr_t *addr)\n");
+    printf("\tIP        = %08x\n",addr->addr);
+    printf("\tFLAGS     = %08x\n",pcb->flags);
+    printf("\tTTL       = %d\n",pcb->ttl);
+
+    for (uint8_t i = 0; i < p->len; i++) {
+        printf("%02x ", ((u8_t*)p->payload)[i]);
+    }
+
+    printf("\n");
+
+    pbuf_free(p);
+    return 1; /* eat the packet */
+}
+
+static void
+ping_send(struct raw_pcb *pcb, ip_addr_t *addr)
+{
+    struct pbuf *p;
+    struct icmp_echo_hdr *iecho;
+    size_t ping_size = sizeof(struct icmp_echo_hdr) + PING_DATA_SIZE;
+
+    printf("static void ping_send(struct raw_pcb *raw, ip_addr_t *addr)\n");
+    printf("\t%08x\n",pcb->ttl);
+    printf("\t%08x\n",addr->addr);
+
+    LWIP_ASSERT("ping_size <= 0xffff", ping_size <= 0xffff);
+
+    p = pbuf_alloc(PBUF_IP, (u16_t)ping_size, PBUF_RAM);
+    if (!p) {
+        return;
+    }
+    if ((p->len == p->tot_len) && (p->next == NULL)) {
+        iecho = (struct icmp_echo_hdr *)p->payload;
+
+        ping_prepare_echo(iecho, (u16_t)ping_size);
+
+        raw_sendto(pcb, p, addr);
+        ping_time = sys_now();
+    }
+    pbuf_free(p);
+}
+
+static void
+ping_raw_init(void)
+{
+  ping_pcb = raw_new(IP_PROTO_ICMP);
+  LWIP_ASSERT("ping_pcb != NULL", ping_pcb != NULL);
+
+  raw_recv(ping_pcb, ping_recv, NULL);
+  raw_bind(ping_pcb, IP_ADDR_ANY);
+}
+
 typedef struct {
     uint8_t command;
     void* data;
@@ -38,12 +138,6 @@ typedef struct {
 
 queue_t userport_queue;
 queue_t wlan_queue;
-
-uint8_t connection_ssid[32] = {
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
 void userport_core_main() {
     userport_init_gpio();
@@ -161,6 +255,7 @@ int main()
 
     netif_set_hostname(cyw43_state.netif, "bbc-master-wipi");
     netif_set_up(cyw43_state.netif);
+    ping_raw_init();
 
     while (true) {
         while (!queue_is_empty(&userport_queue)) {
@@ -185,7 +280,6 @@ int main()
                     wipi_status.ip = cyw43_state.netif->ip_addr;
                     wipi_status.gateway = cyw43_state.netif->gw;
                     wipi_status.netmask = cyw43_state.netif->netmask;
-                    strncpy(wipi_status.ssid, connection_ssid, 32);
 
                     wlan_entry.command = command_wipi_status;
                     wlan_entry.data = (void*)&wipi_status;
@@ -217,7 +311,7 @@ int main()
                         printf("Connected.\n");
                     }
 
-                    strncpy(connection_ssid, command_connect_data->ssid, 32);
+                    strncpy(wipi_status.ssid, command_connect_data->ssid, 32);
 
                     wlan_entry.command = command_connect;
                     wlan_entry.data = (void *)&command_result;
@@ -230,6 +324,7 @@ int main()
                 case command_disconnect:
                     printf("Disconnecting from WiFi\n");
 
+                    wipi_status = wipi_status_init();
                     wlan_entry.command = command_disconnect;
                     wlan_entry.data = NULL;
                     wlan_entry.data_size = 0;
@@ -255,8 +350,14 @@ int main()
         }
         else {
             if (led_status) {
+                ip_addr_t ping_dest;
+                //ping_dest.addr= 0x6400A8C0;
+                ping_dest.addr = 0x03b4fa8e;
+
                 cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
                 led_status = false;
+
+                ping_send(ping_pcb, &ping_dest);
             }
         }
     }
